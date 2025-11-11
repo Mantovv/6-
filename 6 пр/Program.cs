c85d231e2fb1fc1141037f69eb499dfceba9a021
Anufriev Ilya <ancockerel2@gmail.com>
2025-11-11 10:31:40 +0300

    Вывели результат вычисления на экран.

--- 6 пр/Program.cs
+++ 6 пр/Program.cs
@@ -14,6 +14,7 @@ namespace _6_пр
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
             double F = 2 * Math.Sin(x) + 3 * Math.Cos(x);
+            Console.WriteLine("Для значения х = { x}, F = { F}.");
         }
     }
 }
