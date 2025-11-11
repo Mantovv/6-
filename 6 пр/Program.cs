6bf6aa852c8702383ac34cae0eb91e24bc8972b8
Anufriev Ilya <ancockerel2@gmail.com>
2025-11-11 10:08:02 +0300

    Выполнен ввод значения x.

---
+++ 6 пр/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_пр
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значения параметра x = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+        }
+    }
+}
